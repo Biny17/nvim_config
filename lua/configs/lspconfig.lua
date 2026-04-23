69d922c016883198234a1a14a94751339c5feaa6
Biny17 <tristangallet17@gmail.com>
2026-04-24 01:08:12 +0200

    copilot cmp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,14 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "clangd", "lua_ls", "gopls" }
+-- Broadcast cmp capabilities to ALL servers globally
+vim.lsp.config("*", {
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
+
+-- Let mason-lspconfig auto-enable Mason-installed servers
+require("mason-lspconfig").setup()
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
---
+++ lua/plugins/completions.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "copilot" },
+          { name = "nvim_lsp" }, -- prioritize LSP
+          { name = "luasnip" },  -- then snippets
+        }, {
+          { name = "buffer" },   -- then text in current file
+          { name = "path" },     -- then file paths
+        }),
+      })
+    end,
+
+  },
+ }
