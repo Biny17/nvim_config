367ea57aacf7213a384d3e139f646ec31d006e3e
Biny17 <tristangallet17@gmail.com>
2026-04-28 01:35:54 +0200

    ADD: flash surround go

---
+++ lua/plugins/go.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("go").setup()
+      local fmt_grp = vim.api.nvim_create_augroup("goimports", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require("go.format").goimports()
+        end,
+        group = fmt_grp,
+      })
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()',
+  },
+}
--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,6 +1,7 @@
 return {
   {
     "echasnovski/mini.surround",
+    keys = { "sa", "sd", "sf", "sF", "sh", "sr", "sn" },
     opts = {},
   },
 }
