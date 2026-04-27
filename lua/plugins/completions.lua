866d9d5fa66a3cfecc08a5006d89cb6d2bfd1cd7
Biny17 <tristangallet17@gmail.com>
2026-04-27 21:30:49 +0200

    surround

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -37,7 +37,7 @@ return {
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          { name = "copilot", group_index = 2},
+          -- { name = "copilot", group_index = 2},
           { name = "nvim_lsp", group_index = 2 },
           { name = "luasnip", group_index = 2 },
         }, {
