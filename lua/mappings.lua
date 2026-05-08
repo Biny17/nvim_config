6d119bb7dd42d1ce6e458cdb83807960063a427d
Tristan <tgallet@student.42.fr>
2026-05-08 16:49:35 +0200

    gs -> lsp doc symbols

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "<C-d>", "<ESC>viw", { desc = "Visual select current word" })
 
@@ -12,5 +12,6 @@ map("i", "<C-d>", "<ESC>viw", { desc = "Visual select current word" })
 map('t', '<Esc>', '<C-\\><C-n>')
 
 -- Standard Telescope LSP references
-vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = 'LSP References' })
+map('n', 'gr', require('telescope.builtin').lsp_references, { desc = 'LSP References' })
+map('n', 'gs', require('telescope.builtin').lsp_document_symbols, { desc = 'LSP Symbols'})
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
